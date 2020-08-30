862164c168e7d62b070c93f64b19cfa207d04451
Mohammad-Eshtaiwi <mohd.eshtiwi@gmail.com>
2020-08-30 16:17:25 +0300

    feature 3

--- location.js
+++
@@ -1,7 +0,0 @@
-export default function Location(city, data) {
-  let { display_name: formatted_query, lat: latitude, lon: longitude } = data;
-  this.city = city;
-  this.formatted_query = formatted_query;
-  this.latitude = latitude;
-  this.longitude = longitude;
-}
