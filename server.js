56452cebde8729755d7fc57b7c99d6dfb95887ae
Mohammad-Eshtaiwi <mohd.eshtiwi@gmail.com>
2020-08-31 17:25:17 +0300

    feature 6 done

--- server.js
+++ server.js
@@ -56,7 +56,48 @@ function Weather(data) {
     this.forecast = weather.description;
     this.time = time;
 }
+// TRAIL
 
+app.get("/trails", (req, res) => {
+    const url = `https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&key=${process.env.TRAIL_API_KEY}`;
+    superagent
+        .get(url)
+        .then(({ body }) => {
+            let result = body.trails.map((item) => new Trails(item));
+            console.log(result);
+            res.send(result);
+        })
+        .catch((Error) => {
+            console.log(Error);
+        });
+});
+function Trails(data) {
+    let {
+        name,
+        location,
+        length,
+        stars,
+        starVotes: star_votes,
+        summary,
+        url: trail_url,
+        conditionStatus,
+        conditionDetails,
+        conditionDate,
+    } = data;
+
+    let date = conditionDate.split(" ");
+    console.log(data.name);
+    this.name = name;
+    this.location = location;
+    this.length = length;
+    this.stars = stars;
+    this.star_votes = star_votes;
+    this.summary = summary;
+    this.trail_url = trail_url;
+    this.conditions = conditionStatus + ": " + conditionDetails;
+    this.condition_date = date[0];
+    this.condition_time = date[1];
+}
 // error
 app.use(function (req, res, next) {
     // Do logging and user-friendly error message display.
