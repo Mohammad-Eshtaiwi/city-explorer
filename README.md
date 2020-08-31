e4bb0d5f2410801e57804acb9720679e7af8a230
Mohammad-Eshtaiwi <mohd.eshtiwi@gmail.com>
2020-08-31 15:08:38 +0300

    start feature 5

--- README.md
+++ README.md
@@ -4,27 +4,38 @@
 
 **feature #1**
 
-- Estimate of time needed to complete: hour
-- Start time: 1:00 pm
+-   Estimate of time needed to complete: hour
+-   Start time: 1:00 pm
 
-- Finish time: 2:00
+-   Finish time: 2:00
 
-- Actual time needed to complete: hour
+-   Actual time needed to complete: hour
 
-  **feature #2**
+    **feature #2**
 
-- Estimate of time needed to complete: hour
-- Start time: 2:00 pm
+-   Estimate of time needed to complete: hour
+-   Start time: 2:00 pm
 
-- Finish time: 2:45 pm
+-   Finish time: 2:45 pm
 
-- Actual time needed to complete: 45 min
+-   Actual time needed to complete: 45 min
 
-  **feature #3**
+    **feature #3**
 
-- Estimate of time needed to complete: hour
-- Start time: 2:45 pm
+-   Estimate of time needed to complete: hour
+-   Start time: 2:45 pm
 
-- Finish time: 4:00 pm
+-   Finish time: 4:00 pm
 
-- Actual time needed to complete: 45 min
+-   Actual time needed to complete: 45 min
+
+# day 2
+
+**feature #4**
+
+-   Estimate of time needed to complete: 30 min
+-   Start time: 2:30 pm
+
+-   Finish time: 3:00 pm
+
+-   Actual time needed to complete: 30 min
--- server.js
+++ server.js
@@ -3,52 +3,59 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+require("dotenv").config();
+const superagent = require("superagent");
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.get("/location", (req, res) => {
-  let data = require("./data/location.json");
-  let city = req.query.city;
+    let city = req.query.city;
+    const url = `https://eu1.locationiq.com/v1/search.php?key=${process.env.LOCATION_KEY}&q=${city}&format=json`;
+    superagent.get(url).then(({ body }) => {
+        let location = new Location(city, body);
 
-  let location = new Location(city, data[0]);
-  res.send(location);
+        res.send(location);
+    });
 });
 
 function Location(city, data) {
-  let { display_name: formatted_query, lat: latitude, lon: longitude } = data;
+    let {
+        display_name: formatted_query,
+        lat: latitude,
+        lon: longitude,
+    } = data[0];
 
-  this.city = city;
-  this.formatted_query = formatted_query;
-  this.latitude = latitude;
-  this.longitude = longitude;
+    this.city = city;
+    this.formatted_query = formatted_query;
+    this.latitude = latitude;
+    this.longitude = longitude;
 }
 
 // Weather
 
 app.get("/weather", (req, res) => {
-  let { data } = require("./data/weather.json");
-  let result = [];
-  data.forEach((one) => {
-    result.push(new Weather(one));
-  });
-  res.send(result);
+    let { data } = require("./data/weather.json");
+
+    let result = data.map((one) => {
+        return new Weather(one);
+    });
+    res.send(result);
 });
 function Weather(data) {
-  let { weather, datetime: time } = data;
-  this.forecast = weather.description;
-  this.time = time;
+    let { weather, datetime: time } = data;
+    this.forecast = weather.description;
+    this.time = time;
 }
 
 // error
 app.use(function (req, res, next) {
-  // Do logging and user-friendly error message display.
-  console.log("Route does not exist");
-  res.status(500).send("Sorry, something went wrong");
+    // Do logging and user-friendly error message display.
+
+    res.status(500).send("Sorry, something went wrong");
 });
 
 app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
-
-//test
